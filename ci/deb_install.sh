bd34af8cd01cec3b2966e030685764d2e5d639ac
Ren Wang <iamwrm@gmail.com>
2022-01-27 12:56:49 +0000

    fixing git depend

--- ci/deb_install.sh
+++ ci/deb_install.sh
@@ -1,8 +1,8 @@
 apt update -y && apt upgrade -y
 
-apt install -y gcc-10 g++-10 build-essential ninja-build cmake clang
+apt install -y build-essential ninja-build cmake git curl
 apt install -y python3-pip
-apt install -y clang-13 lld-13 curl
+apt install -y clang-13 lld-13 gcc-10 g++-10 
 
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 PATH="$HOME/.cargo/bin:$PATH"
