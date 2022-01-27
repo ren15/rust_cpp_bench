05e065f7c7afc257b6eb083be335acf427070c5b
Ren Wang <iamwrm@gmail.com>
2022-01-27 13:12:45 +0000

    add cbindgen

--- ci/deb_install.sh
+++ ci/deb_install.sh
@@ -7,5 +7,6 @@ apt install -y clang-13 lld-13 gcc-10 g++-10
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 PATH="$HOME/.cargo/bin:$PATH"
 rustup install nightly
+cargo install -f cbindgen
 
 pip install conan
