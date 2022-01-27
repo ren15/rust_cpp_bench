983392d0dc6cdd53cbacc5c9f4c3ccd6cbf10180
Ren Wang <iamwrm@gmail.com>
2022-01-27 08:38:23 +0000

    rust is_prime is generic, get_prime still working

--- my_rust_math/src/prime_num.rs
+++ my_rust_math/src/prime_num.rs
@@ -2,7 +2,7 @@
 pub extern "C" fn get_prime_cnt_leq_u32(num: u32) -> u32 {
     let mut cnt = 0;
     for i in 2..=num {
-        if is_prime_u32(i) {
+        if is_prime(i) {
             cnt += 1;
         }
     }
@@ -13,7 +13,7 @@ pub extern "C" fn get_prime_cnt_leq_u32(num: u32) -> u32 {
 pub extern "C" fn get_prime_cnt_leq_u16(num: u16) -> u16 {
     let mut cnt = 0;
     for i in 2..=num {
-        if is_prime_u16(i) {
+        if is_prime(i) {
             cnt += 1;
         }
     }
@@ -31,41 +31,13 @@ pub extern "C" fn get_prime_cnt_leq(num: u64) -> u64 {
     cnt
 }
 
-fn is_prime(num: u64) -> bool {
-    if num == 2 {
-        return true;
-    }
-    if num % 2 == 0 {
-        return false;
-    }
-    let mut i = 3;
-    while i * i <= num {
-        if num % i == 0 {
-            return false;
-        }
-        i += 2;
-    }
-    true
-}
+use num_traits::{FromPrimitive, PrimInt, ToPrimitive, Unsigned};
 
-fn is_prime_u32(num: u32) -> bool {
-    if num == 2 {
-        return true;
-    }
-    if num % 2 == 0 {
-        return false;
-    }
-    let mut i = 3;
-    while i * i <= num {
-        if num % i == 0 {
-            return false;
-        }
-        i += 2;
-    }
-    true
-}
-
-fn is_prime_u16(num: u16) -> bool {
+fn is_prime<T>(num: T) -> bool
+where
+    T: PrimInt + Unsigned + FromPrimitive + ToPrimitive,
+{
+    let num = num.to_u64().unwrap();
     if num == 2 {
         return true;
     }
